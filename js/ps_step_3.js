2c0ea2526ebab3248e94939642ba219d7f65c65c
RioFabrice <fabrice.rio@hfg.design>
2022-03-03 13:27:26 +0100

    add function onMouseClick
    - add EventListener
    - add functionOnMouseClick
    - add random rectangle selection
    - color array
    - color array in nested loop
    Fixes
    - for loop in setup

--- js/ps_step_3.js
+++ js/ps_step_3.js
@@ -1,22 +1,22 @@
 Homeworks.aufgabe = 3;
 
-/*
-let hugoNumber = 13.1; // Integer, Float, Double, ...
-let hugoString = "Text";
-let hugoBoolean = true;
-let hugoArray = ["Benno", "Marina", "Elijah", "Selina"];
-*/
-
-let numCol = 14;
-let numRow = 8;
+let numCol = 10;
+let numRow = 10;
 let borderX, borderY;
-let length = 50;
-let gap = 5;
+let length = 30;
+let gap = 10;
+let colors = []
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('thecanvas');
-  //document.addEventListener('keyup', onKeyUp);
+  for (let col = 0; col < numCol; col++) {
+    colors[col] = [];
+    for (let row = 0; row < numRow; row++) {
+      colors[col][row] = "white";
+    }
+  }
+  document.addEventListener("click", onMouseClick);
   borderX = (windowWidth - (numRow * (length + gap) - gap)) /2;
   borderY = (windowHeight - (numCol * (length + gap) - gap)) /2;
 }
@@ -24,11 +24,18 @@ function setup() {
 function draw() {
   for (let col = 0; col < numCol; col++) {
     for (let row = 0; row < numRow; row++) {
+      fill(colors[col][row]);
       rect(borderX + row * (length + gap), borderY + col * (length + gap), length, length);
     }
   }
 }
 
+function onMouseClick() {
+  let randomX = Math.floor(Math.random() * numCol);
+  let randomY = Math.floor(Math.random() * numRow);
+  colors[randomX][randomY] = "black"
+  console.log("click")
+}
 
 //Hide the HTML Text with h and show it again with s
 function keyTyped() {
